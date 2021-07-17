109604e93810759141189c041d36d7a2aa0430a9
Nnanna <nnanna.uwakwe@outlook.com>
2021-07-17 13:19:14 +0100

    Refactored code to solve scope issues, removed fighterStart variable from gameStart function and named it a global variable. Player & fighter creation moved to separate functions which are now called within the gameStart function.

--- Scripts/app.js
+++ Scripts/app.js
@@ -5,31 +5,66 @@ const score = document.querySelector('#score span')
 const lifeCount = document.querySelector('#life-count span')
 const cells = []
 
-// * grid variables
+// * Grid Variables
 const width = 10
 const cellCount = width * width
 
-
-// * functions
+// * Building the Grid
 function createGrid () {
   for (let i = 0; i < cellCount; i++) {
     const cell = document.createElement('div')
     cell.textContent = i
     gameGrid.appendChild(cell)
     cells.push(cell)
-  }
-  let playerStart = cells[94].classList.add('player')
-  let fighterStart = [cells[12], cells[13], cells[14], cells[15], cells[16], cells[22], cells[23], cells[24], cells[25], cells[26], cells[32], cells[33], cells[34], cells[35], cells[36]]
+  }  
+}
+
+createGrid()
+
+// * Game Variables
+const fighterStart = [cells[12], cells[13], cells[14], cells[15], cells[16], cells[22], cells[23], cells[24], cells[25], cells[26], cells[32], cells[33], cells[34], cells[35], cells[36]]
+
+// * Functions
+function createFighters () {
   fighterStart.forEach(fighter => {
     fighter.classList.add('fighter')
   })
 }
 
-function gameStart () {
-  createGrid()
-  score.innerHTML = 0
-  lifeCount.innerHTML = 0
+function createPlayer () {
+  return cells[94].classList.add('player')
 }
 
-// * events
+function removeFighters () {
+  const fighterPosition = fighterStart
+  fighterPosition.forEach(fighter => {
+    fighter.classList.remove('fighter')
+  })
+}
+
+function addFighters () {
+  
+}
+
+function fighterMovement () {
+  setInterval(() => {
+    console.log('move fighters')
+  }, 1000)
+}
+
+function gameStart () {  
+  score.innerHTML = 0
+  lifeCount.innerHTML = 0
+  createFighters()
+  createPlayer()
+  fighterMovement()
+}
+
+
+function handleKeyUp (e) {
+  console.log('Key Up!', e.keyCode)  
+}
+
+// * Events
 startBtn.addEventListener('click', gameStart)
+window.addEventListener('keyup', handleKeyUp)
