5586558d2d12ec72aed103f8cdaa7ff2ae44b91c
Nnanna <nnanna.uwakwe@outlook.com>
2021-07-17 08:39:53 +0100

    Start grid with start btn, score & lives counters and player start position all hooked up. The fighter grid starting position needs to be hooked into either the createGrid function or the gameStart function. At present trying to declare a new fighterStart array may need to filter out a new one.

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,1 +1,36 @@
-console.log('Hello world')
+// * DOM Elelments
+const gameGrid = document.querySelector('.game-grid')
+const startBtn = document.querySelector('#start')
+const score = document.querySelector('#score span')
+const lifeCount = document.querySelector('#life-count span')
+const cells = []
+
+// * grid variables
+const width = 10
+const cellCount = width * width
+
+
+// * functions
+function createGrid () {
+  for (let i = 0; i < cellCount; i++) {
+    const cell = document.createElement('div')
+    cell.textContent = i
+    gameGrid.appendChild(cell)
+    cells.push(cell)
+  }
+  let playerStart = cells[94].classList.add('player')
+  let fighterStart = [cells[13], cells[14], cells[15], cells[16], cells[23], cells[24], cells[25], cells[26], cells[33], cells[34], cells[35], cells[36]].forEach(fighter => {
+    fighter.classlist.add('fighter')
+  })
+}
+
+
+
+function gameStart () {
+  createGrid()
+  score.innerHTML = 0
+  lifeCount.innerHTML = 0
+}
+
+// * events
+startBtn.addEventListener('click', gameStart)
