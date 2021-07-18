4ccc1b68ee08e45fad17505f94158981d01dd416
Nnanna <nnanna.uwakwe@outlook.com>
2021-07-18 16:52:08 +0100

    added a bit more organisation to the codebase, still need to finalise the fleet movement proving to be pretty tricky

--- Scripts/app.js
+++ Scripts/app.js
@@ -24,11 +24,13 @@ createGrid()
 // * Game Variables
 const fighterStart = [cells[12], cells[13], cells[14], cells[15], cells[16], cells[22], cells[23], cells[24], cells[25], cells[26], cells[32], cells[33], cells[34], cells[35], cells[36]]
 
-const fighterPosition = [12, 13, 14, 15, 16, 22, 23, 24, 25, 26, 32, 33, 34, 35, 36]
+let fighterPosition = [12, 13, 14, 15, 16, 22, 23, 24, 25, 26, 32, 33, 34, 35, 36]
 
 let playerPosition = 94
 
 // * Functions
+
+//* Create Fighters & Player
 function createFighters () {
   fighterStart.forEach(fighter => {
     fighter.classList.add('fighter')
@@ -39,24 +41,48 @@ function createPlayer () {
   return cells[94].classList.add('player')
 }
 
+// * Fighter Fleet Movement
 function removeFighters () {
-  fighterPosition.forEach(fighter => {
+  fighterStart.forEach(fighter => {
     fighter.classList.remove('fighter')
   })
 }
 
 function addFighters () {
+  fighterPosition.forEach(fighter => {
+    fighter.classList.add('fighter')
+  })
+}
 
+function moveRight () {
+  fighterPosition.map(fighter => {
+    return fighter += 1   
+  })
+}
+
+function moveLeft () {
+  fighterPosition.map(fighter => {
+    return fighter -= 1   
+  })
+}
+
+function moveDown () {
+  fighterPosition.map(fighter => {
+    return fighter += 10
+  })
 }
 
 function fighterMovement () {
-
+  setInterval(() => {
+    // move right till x < width - 1
+    // move down 1 row
+    // move left until x > 0
+    // move down 1 row
+    // repeat
+  }, 1000) 
 }
 
-function playerMovement () {
-
-}
-
+// * Game Start
 function gameStart () {  
   score.innerHTML = 0
   lifeCount.innerHTML = 0
@@ -65,6 +91,18 @@ function gameStart () {
   fighterMovement()
 }
 
+// * Laser Fire
+function laserFire () {
+  setInterval(()=> {
+    // keyCode event 'keyup' for x
+    // move up from playerPosition until:
+      // hit a fighter
+      // reach top row
+    // remove
+  })
+}
+
+// * Player Movement
 function addPlayer () {
   cells[playerPosition].classList.add('player')
 }
@@ -73,22 +111,24 @@ function removePlayer () {
   cells[playerPosition].classList.remove('player')
 }
 
-
-function handleKeyUp (e) {
-  
+function movePlayer (e) {  
+  const x = playerPosition % width
   removePlayer()
   switch(e.keyCode) {
     case 39:
-      playerPosition++  
+      if (x < width - 1) {
+        playerPosition++ 
+      }       
       break
     case 37:
-      playerPosition--
+      if (x > 0) {
+        playerPosition--
+      }      
       break    
   }
   addPlayer()
-
 }
 
 // * Events
 startBtn.addEventListener('click', gameStart)
-window.addEventListener('keyup', handleKeyUp)
+window.addEventListener('keyup', movePlayer)
